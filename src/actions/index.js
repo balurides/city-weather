1a61871c3f49e19c42f8ece9911a45674aae28f6
baluonline <baluonline@Gmail.com>
2019-03-20 08:24:06 -0500

    added albums

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
-
-import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceHolder';
 
 export const fecthUsers = () => async dispatch => {
-    const response = await jsonPlaceHolder.get('/users');
+    const response = await jsonPlaceholder.get('/users');
     dispatch({
         type:'FETCH_USERS',
         payload:response.data
-    })
-}
+    });
+};
+    
+export const fetchAlbums = userId => async dispatch => {
+    const response1 = await jsonPlaceholder.get(`/albums/userId=${userId}`);
+    dispatch({
+        type:'FETCH_ALBUMS',
+        payload:response1.data
+    });
+};
+    
